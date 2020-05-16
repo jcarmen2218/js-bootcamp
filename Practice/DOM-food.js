12ff948fb8b4cf1dd3b5dcda54cfd9b7a32737ac
jcarmen2218 <64708790+jcarmen2218@users.noreply.github.com>
2020-05-16 10:51:13 -0700

    Finished Grocery List Alphabetical

--- Practice/DOM-food.js
+++ Practice/DOM-food.js
@@ -38,9 +38,62 @@ const togglefood = function (id) {
     }
 }
 
+ // Get the DOM elements for an individual note
+ // generatefoodDOM
+ const generatefoodDOM = function (food) {
+    const foodE1 = document.createElement ('div')
+    const checkbox = document.createElement('input')
+    const foodText = document.createElement('span')
+    const button = document.createElement('button')
+
+    // Set up food checkbox
+    checkbox.setAttribute('type', 'checkbox')
+    checkbox.checked = food.Checked
+    foodE1.appendChild(checkbox)
+    checkbox.addEventListener('change', function () {
+        togglefood(food.id)
+        saveGrocery(grocery)
+        renderGrocery(grocery, filter)
+    })
+
+    // Setup the food text
+    foodText.textContent = food.food
+    foodE1.appendChild(foodText)
+
+ // Setup the remove food button
+ button.textContent = 'x'
+ foodE1.appendChild(button)
+ button.addEventListener('click', function(){
+     removeGrocery(food.id)
+     saveGrocery(grocery)
+     renderGrocery(grocery, filter)
+ })
+
+    return foodE1
+ }
+
+
+ // Sort your groceries by one of three ways
+const sortGrocery = function (grocery, sortBy) {
+     if (sortBy ==='alphabetical') {
+        return grocery.sort(function (a,b) {
+            if (a.food.toLowerCase() < b.food.toLowerCase()) {
+                return -1
+            } else if  (a.food.toLowerCase() > b.food.toLowerCase()) {
+                return 1 
+            } else {
+                return 0
+            }
+        })
+     } else {
+         return grocery
+     }
+}
+
 // Render application grocery based on filter
 // renderGrocery
 const renderGrocery= function(grocery, filter) {
+    grocery = sortGrocery(grocery, filter.sortBy)
     const filteredGrocery = grocery.filter(function (food){
         const searchTextMatch = food.food.toLowerCase().includes(filter.searchText.toLowerCase())
         const hideCheckedMatch = !filter.hideChecked || !food.Checked
@@ -60,69 +113,6 @@ const renderGrocery= function(grocery, filter) {
    })
  }
 
- // Get the DOM elements for an individual note
- // generatefoodDOM
- const generatefoodDOM = function (food) {
-    const foodE1 = document.createElement ('div')
-    const checkbox = document.createElement('input')
-    const foodText = document.createElement('span')
-
-    // Set up food checkbox
-    checkbox.setAttribute('type', 'checkbox')
-    checkbox.checked = food.Checked
-    foodE1.appendChild(checkbox)
-    checkbox.addEventListener('change', function () {
-        togglefood(food.id)
-        saveGrocery(grocery)
-        renderGrocery(grocery, filter)
-    })
-
-    // Setup the food text
-    foodText.textContent = food.food
-    foodE1.appendChild(foodText)
-
-    return foodE1
- }
-
- // Sort your groceries by one of three ways
-const sortGrocery = function (food, sortBy) {
-    if (sortBy === 'byEdited') {
-        return food.sort(function (a, b) {
-            if (a.updatedAt > b.updatedAt) {
-                return -1
-            } else if (a.updatedAt < b.updatedAt) {
-                return 1
-            } else {
-                return 0
-            }
-            
-        })
-
-     } else if (sortBy === 'byCreated') {
-         return food.sort(function (a, b) {
-             if (a.createdAt > b.createdAt) {
-                 return -1
-             } else if (a.createdAt < b.createdAt) {
-                return 1
-             } else {
-                return 0
-             }
-        })
-     } else if (sortBy ==='alphabetical') {
-        return food.sort(function (a,b) {
-            if (a.title.toLowerCase() < b.title.toLowerCase()) {
-                return -1
-            } else if  (a.title.toLowerCase() > b.title.toLowerCase()) {
-                return 1 
-            } else {
-                return 0
-            }
-        })
-     } else {
-         return food
-     }
-}
-
  // Get the DOM elements for list summary
  // generateSummaryDOM
  const generateSummaryDOM = function (itemLeft) {
@@ -130,8 +120,3 @@ const sortGrocery = function (food, sortBy) {
      summary.textContent = `You have ${itemLeft.length} food items left on your shopping list`
      return summary
  }
- 
- // Generate the last edited message
-const generateLastEdited = function (timestamp) {
-    return `Last edited ${moment(timestamp).fromNow()}`
-}
--- Practice/DOM.js
+++ Practice/DOM.js
@@ -3,7 +3,7 @@ let grocery = getSavedgrocery()
 const filter = {
    searchText:'',
    hideChecked: false,
-   sortBy: 'byEdited'
+   sortBy: 'alphabetical'
 }
 
 renderGrocery(grocery, filter)
@@ -16,17 +16,18 @@ document.querySelector('#food-text').addEventListener('input', function(e) {
 document.querySelector('#shop-text').addEventListener('submit', function(e){
    e.preventDefault()
    const id = uuidv4()
-   const timestamp = moment().valueOf()
    grocery.push({
       id: id,
-      title: '',
-      body: '',
-      createdAt: timestamp, 
-      updatedAt: timestamp
+      food:e.target.elements.grocery.value,
    })
    saveGrocery(grocery)
    renderGrocery(grocery, filter)
    e.target.elements.grocery.value=''
+})
+
+document.querySelector('#filter-by').addEventListener('change', function(e){
+   filter.sortBy = e.target.value
+   renderGrocery(grocery, filter)
 })
 
 document.querySelector('#hide-checked').addEventListener('change', function(e){
